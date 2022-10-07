b21017ba552b592fb4fbf256c58f529722d38c6e
Insop Song <insop.song@gmail.com>
2022-10-06 20:09:09 -0700

    Update from Fall 2021

--- src/submission.py
+++ src/submission.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 
 import random
+from typing import Callable, Dict, List, Tuple, TypeVar, DefaultDict
 import collections
 import math
 import sys
 from util import *
+
+FeatureVector = Dict[str, int]
+WeightVector = Dict[str, float]
+Example = Tuple[FeatureVector, int]
 
 ############################################################
 # Problem 1: binary classification
@@ -13,11 +18,12 @@ from util import *
 ############################################################
 # Problem 1a: feature extraction
 
-def extractWordFeatures(x):
+
+def extractWordFeatures(x: str) -> FeatureVector:
     """
     Extract word features for a string x. Words are delimited by
     whitespace characters only.
-    @param string x: 
+    @param string x:
     @return dict: feature vector representation of x.
     Example: "I am what I am" --> {'I': 2, 'am': 2, 'what': 1}
     """
@@ -25,60 +31,103 @@ def extractWordFeatures(x):
     # ### START CODE HERE ###
     # ### END CODE HERE ###
 
+
 ############################################################
 # Problem 1b: stochastic gradient descent
 
-def learnPredictor(trainExamples, testExamples, featureExtractor, numIters, eta):
-    '''
-    Given |trainExamples| and |testExamples| (each one is a list of (x,y)
-    pairs), a |featureExtractor| to apply to x, and the number of iterations to
-    train |numIters|, the step size |eta|, return the weight vector (sparse
+T = TypeVar("T")
+
+
+def learnPredictor(
+    trainExamples: List[Tuple[T, int]],
+    validationExamples: List[Tuple[T, int]],
+    featureExtractor: Callable[[T], FeatureVector],
+    numEpochs: int,
+    eta: float,
+) -> WeightVector:
+    """
+    Given |trainExamples| and |validationExamples| (each one is a list of (x,y)
+    pairs), a |featureExtractor| to apply to x, and the number of epochs to
+    train |numEpochs|, the step size |eta|, return the weight vector (sparse
     feature vector) learned.
 
     You should implement stochastic gradient descent.
 
-    Note: only use the trainExamples for training!
-    You should call evaluatePredictor() on both trainExamples and testExamples
-    to see how you're doing as you learn after each iteration.
-    '''
+    Notes:
+    - Only use the trainExamples for training!
+    - You should call evaluatePredictor() on both trainExamples and validationExamples
+    to see how you're doing as you learn after each epoch.
+    - The predictor should output +1 if the score is precisely 0.
+    """
     weights = {}  # feature => weight
     # ### START CODE HERE ###
     # ### END CODE HERE ###
     return weights
 
+
 ############################################################
 # Problem 1c: generate test case
 
-def generateDataset(numExamples, weights):
-    '''
+
+def generateDataset(numExamples: int, weights: WeightVector) -> List[Example]:
+    """
     Return a set of examples (phi(x), y) randomly which are classified correctly by
     |weights|.
-    '''
+    """
     random.seed(42)
+
     # Return a single example (phi(x), y).
     # phi(x) should be a dict whose keys are a subset of the keys in weights
-    # and values can be anything (randomize!) with a nonzero score under the given weight vector.
+    # and values can be anything (randomize!) with a score for the given weight vector.
     # y should be 1 or -1 as classified by the weight vector.
+    # y should be 1 if the score is precisely 0.
+
+    # Note that the weight vector can be arbitrary during testing.
     def generateExample():
         phi = None
         y = None
         # ### START CODE HERE ###
         # ### END CODE HERE ###
         return (phi, y)
+
     return [generateExample() for _ in range(numExamples)]
 
-############################################################
-# Problem 1e: character features
 
-def extractCharacterFeatures(n):
-    '''
+############################################################
+# Problem 1d: character features
+
+
+def extractCharacterFeatures(n: int) -> Callable[[str], FeatureVector]:
+    """
     Return a function that takes a string |x| and returns a sparse feature
     vector consisting of all n-grams of |x| without spaces mapped to their n-gram counts.
     EXAMPLE: (n = 3) "I like tacos" --> {'Ili': 1, 'lik': 1, 'ike': 1, ...
     You may assume that n >= 1.
-    '''
+    """
+
     def extract(x):
         pass
         # ### START CODE HERE ###
         # ### END CODE HERE ###
+
     return extract
+
+
+############################################################
+# Problem 2b: K-means
+############################################################
+
+
+def kmeans(
+    examples: List[Dict[str, float]], K: int, maxEpochs: int
+) -> Tuple[List, List, float]:
+    """
+    examples: list of examples, each example is a string-to-float dict representing a sparse vector.
+    K: number of desired clusters. Assume that 0 < K <= |examples|.
+    maxEpochs: maximum number of epochs to run (you should terminate early if the algorithm converges).
+    Return: (length K list of cluster centroids,
+            list of assignments (i.e. if examples[i] belongs to centers[j], then assignments[i] = j),
+            final reconstruction loss)
+    """
+    # ### START CODE HERE ###
+    # ### END CODE HERE ###
